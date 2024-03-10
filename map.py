683d61574ea9e49ad6ca7a3cb926ead7f5fd4ab5
StarkGoku10 <shreyasacharya3000@gmail.com>
2024-03-10 14:22:25 -0400

    updated confusing variable names
    The variable names for pixel values were misleading, corrected them and updated them to the final map.

--- map.py
+++ map.py
@@ -5,7 +5,7 @@ import numpy as np
 map_width = 500
 map_height = 1200
 
-ORANGE = (141, 3, 6)
+NAVY_COLOR = (141, 3, 6)
 # Create a blank white image
 image = np.ones((map_width, map_height, 3), dtype=np.uint8) * 255
 
@@ -20,8 +20,8 @@ PADDING_COLOR = (31,103,255)
 padding = 5
 
 # Draw rectangles
-cv2.rectangle(image, (100 - padding, -50 - padding), (175 + padding, 350 + padding), ORANGE, -1)
-cv2.rectangle(image, (275 - padding, 100 - padding), (350 + padding, 500 + padding), ORANGE, -1)
+cv2.rectangle(image, (100 - padding, -50 - padding), (175 + padding, 350 + padding), NAVY_COLOR, -1)
+cv2.rectangle(image, (275 - padding, 100 - padding), (350 + padding, 500 + padding), NAVY_COLOR, -1)
 
 cv2.rectangle(image, (100, -50), (175, 350), PADDING_COLOR, -1)
 cv2.rectangle(image, (275, 100), (350, 500), PADDING_COLOR, -1)
@@ -36,12 +36,12 @@ for i in range(polygon_sides):
 
 # Draw the polygon
 cv2.fillPoly(image, [np.array(polygon_vertices)], PADDING_COLOR)
-cv2.polylines(image, [np.array(polygon_vertices)], isClosed=True, color=ORANGE, thickness=5)
+cv2.polylines(image, [np.array(polygon_vertices)], isClosed=True, color=NAVY_COLOR, thickness=5)
 
 # Draw U-shaped rectangle
-cv2.rectangle(image, (1020 - padding, 50 - padding), (1100 + padding, 450 + padding), ORANGE, -1)  # Right vertical part of U
-cv2.rectangle(image, (900 - padding, 50 - padding), (1100 + padding, 125 + padding), ORANGE, -1)  # Top vertical part of U
-cv2.rectangle(image, (900 - padding, 375 - padding), (1100 + padding, 450 + padding), ORANGE, -1)  # Bottom Horizontal part of U
+cv2.rectangle(image, (1020 - padding, 50 - padding), (1100 + padding, 450 + padding), NAVY_COLOR, -1)  # Right vertical part of U
+cv2.rectangle(image, (900 - padding, 50 - padding), (1100 + padding, 125 + padding), NAVY_COLOR, -1)  # Top vertical part of U
+cv2.rectangle(image, (900 - padding, 375 - padding), (1100 + padding, 450 + padding), NAVY_COLOR, -1)  # Bottom Horizontal part of U
 
 # Draw padding for U-shaped rectangle
 cv2.rectangle(image, (1020, 50), (1100, 450), PADDING_COLOR, -1)  # Right vertical part of U without padding
