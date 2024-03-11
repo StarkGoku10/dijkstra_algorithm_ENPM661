23f6ce308d485d6d244a68f9f909432bae2d480f
StarkGoku10 <shreyasacharya3000@gmail.com>
2024-03-10 23:44:35 -0400

    dijsktra algorithm without animation video
    the complete implementation of dijkstra algorithm without the video.

--- Dijkstra_algorihtm.py
+++ Dijkstra_algorihtm.py
@@ -1,55 +1,59 @@
+
 import numpy as np
 import math
-import heapq
 import cv2
 import time
+from queue import PriorityQueue
 
+# defining the search space(map)
 map_width = 1200
 map_height = 500
-polygon_sides = 6
-polygon_length = 150
-polygon_bloat = 5
-PADDING_COLOR = (141, 31, 6)
-OBS_COLOR = (31, 103, 255)
-padding = 5
 
-# Create a blank white image
+#mapping the obstacle space, 
+hexagon_sides = 6
+hexagon_vertex_len = 150
+BORDER_COLOR = (141, 31, 6)
+OBS_COLOR = (31, 103, 255)
+border = 5  #thickness of the clearance 
+
+# Creating the background for search map
 image = np.ones((map_height, map_width, 3), dtype=np.uint8) * 255
 
-# Draw black border around the map
+# Drawing border around the map
 border_thickness = 5
-cv2.rectangle(image, (0, map_height - 1), (map_width - 1, 0), PADDING_COLOR, border_thickness)
+cv2.rectangle(image, (0, map_height - 1), (map_width - 1, 0), BORDER_COLOR, border_thickness)
 
-# Draw rectangles with padding
-cv2.rectangle(image, (100 - padding,  0- padding), (175 + padding, 400 + padding), PADDING_COLOR, -1)
-cv2.rectangle(image, (100 , 0), (175 , 400 ), OBS_COLOR, -1)
-cv2.rectangle(image, (275 - padding, map_height - 400 - padding), (350 + padding, map_height + padding), PADDING_COLOR, -1)
+# Drawing the rectangles with border 
+cv2.rectangle(image, (100 - border, 0 - border), (175 + border, 400 + border), BORDER_COLOR, -1)
+cv2.rectangle(image, (100, 0), (175, 400), OBS_COLOR, -1)
+cv2.rectangle(image, (275 - border, map_height - 400 - border), (350 + border, map_height + border), BORDER_COLOR, -1)
 cv2.rectangle(image, (275, map_height - 400), (350, map_height), OBS_COLOR, -1)
 
-# Calculate the vertices of the polygon with padding
-polygon_vertices = []
-for i in range(polygon_sides):
-    angle = np.radians(i * (360 / polygon_sides) + 90)
-    x = 650 + ((polygon_length) * np.cos(angle))
-    y = map_height - (250 + ((polygon_length) * np.sin(angle)))
-    polygon_vertices.append((int(x), int(y)))
+# Plotting the vertices of the hexagon with border
+hexagon_vertices = []
+for i in range(hexagon_sides):
+    angle = np.radians(i * (360 / hexagon_sides) + 90)
+    x = 650 + ((hexagon_vertex_len) * np.cos(angle))
+    y = map_height - (250 + ((hexagon_vertex_len) * np.sin(angle)))
+    hexagon_vertices.append((int(x), int(y)))
 
-# Draw the polygon
-cv2.fillPoly(image, [np.array(polygon_vertices)], OBS_COLOR)
-cv2.polylines(image, [np.array(polygon_vertices)], isClosed=True, color=(141, 31, 6), thickness=5)
+# Draw the hexagon
+cv2.fillPoly(image, [np.array(hexagon_vertices)], OBS_COLOR)
+cv2.polylines(image, [np.array(hexagon_vertices)], isClosed=True, color=(141, 31, 6), thickness=5)
 
-# Draw U-shaped rectangle with padding
-cv2.rectangle(image, (1020 - padding, map_height - 50 + padding), (1100 + padding, map_height - 450 - padding), PADDING_COLOR, -1)  # Right vertical part of U
+# Drawing the inverted C-shaped rectangle with border
+cv2.rectangle(image, (1020 - border, map_height - 50 + border), (1100 + border, map_height - 450 - border), BORDER_COLOR, -1)  
 cv2.rectangle(image, (1020, map_height - 50), (1100, map_height - 450), OBS_COLOR, -1)
-cv2.rectangle(image, (900 - padding, map_height - 50 + padding), (1100 + padding, map_height - 125 - padding), PADDING_COLOR, -1)  # Top vertical part of U
+cv2.rectangle(image, (900 - border, map_height - 50 + border), (1100 + border, map_height - 125 - border), BORDER_COLOR, -1)  
 cv2.rectangle(image, (900, map_height - 50), (1100, map_height - 125), OBS_COLOR, -1)
-cv2.rectangle(image, (900 - padding, map_height - 375 + padding), (1100 + padding, map_height - 450 - padding), PADDING_COLOR, -1)  # Bottom Horizontal part of U
+cv2.rectangle(image, (900 - border, map_height - 375 + border), (1100 + border, map_height - 450 - border), BORDER_COLOR, -1)  
 cv2.rectangle(image, (900, map_height - 375), (1100, map_height - 450), OBS_COLOR, -1)
+
 
 map_width = 1200
 map_height = 500
 
-########## DEFINING A NODE CLASS TO STORE NODES AS OBJECTS ###############
+# Defining a node class to store the coordinates, cost and the index of the parent node.
 
 class Node:
     def __init__(self, x, y, cost, parent_id):
@@ -61,65 +65,62 @@ class Node:
     def __lt__(self, other):
         return self.cost < other.cost
 
-############ CONFIGURATION SPACE CONSTRUCTION WITH OBSTACLES ############
-
+# Configuring the obstacle space and constructing the obstacles
 def Configuration_space(image):
     obs_space = np.zeros((map_height, map_width), dtype=np.uint8)
     for y in range(map_height):
         for x in range(map_width):
-            if np.all(image[y, x] == PADDING_COLOR) or np.all(image[y, x] == OBS_COLOR) or x == 0 or x == map_width - 1 or y == 0 or y == map_height - 1:
+            if np.all(image[y, x] == BORDER_COLOR) or np.all(image[y, x] == OBS_COLOR) or x == 0 or x == map_width - 1 or y == 0 or y == map_height - 1:
                 obs_space[y, x] = 255
     return obs_space
 
 
-############## CHECK IF THE GIVEN MOVE IS VALID OR NOT ###############
 
+ # Check if coordinates are within the boundaries of the obstacle space and if the cell is occupied by an obstacle (value 255)
 def Validity(x, y, obs_space):
-    # Check if coordinates are within the boundaries of the obstacle space and if the cell is occupied by an obstacle (value 255)
     if x < 0 or x >= map_width or y < 0 or y >= map_height or obs_space[y][x] == 255:
         return False
     return True
 
-############## CHECK IF THE GOAL NODE IS REACHED ###############
 
+# checking if the goal node is reached or not
 def Check_goal(present, goal):
     return present.x == goal.x and present.y == goal.y
 
-############# GENERATE UNIQUE KEY ##############
 
+# generating a unique key for each node 
 def key(node):
     return 1022 * node.x + 111 * node.y
 
-############# GENERATE CHILD NODES ##############
-
+# generate child nodes
 def up(x, y, cost):
     return x, y - 1, cost + 1
-
-def down(x, y, cost):
-    return x, y + 1, cost + 1
-
-def left(x, y, cost):
-    return x - 1, y, cost + 1
-
-def right(x, y, cost):
-    return x + 1, y, cost + 1
-
-def bottom_left(x, y, cost):
-    return x - 1, y - 1, cost + 1.414
-
-def bottom_right(x, y, cost):
-    return x + 1, y - 1, cost + 1.414
-
-def up_left(x, y, cost):
-    return x - 1, y + 1, cost + 1.414
 
 def up_right(x, y, cost):
     return x + 1, y + 1, cost + 1.414
 
-######### IMPLEMENTING DIJKSTRA ALGORITHM ##############
+def right(x, y, cost):
+    return x + 1, y, cost + 1
 
+def bottom_right(x, y, cost):
+    return x + 1, y - 1, cost + 1.414
+
+def down(x, y, cost):
+    return x, y + 1, cost + 1
+
+def bottom_left(x, y, cost):
+    return x - 1, y - 1, cost + 1.414
+
+def left(x, y, cost):
+    return x - 1, y, cost + 1
+
+def up_left(x, y, cost):
+    return x - 1, y + 1, cost + 1.414
+
+
+# implementing the djisktra algorithm 
 def dijkstra(start, goal, obs_space):
-    start_time = time.time()  # Start time for measuring time taken
+    start_time = time.time()  # Starting the clock count to calculate the time taken to reach goal node
     if not Validity(start.x, start.y, obs_space):
         print("Invalid start node")
         return None, 0, 0
@@ -135,25 +136,32 @@ def dijkstra(start, goal, obs_space):
     start_node = start
 
     moves = [up, down, left, right, bottom_left, bottom_right, up_left, up_right]
-    unexplored_nodes = {}  # List of all open nodes
+    # Dictionary to store unexplored nodes
+    unexplored_nodes = {}  
 
-    start_key = key(start_node)  # Generating a unique key for identifying the node
+   # Generating a unique key for identifying the node
+    start_key = key(start_node)  
     unexplored_nodes[start_key] = start_node
 
-    explored_nodes = {}  # List of all closed nodes
-    priority_list = []  # List to store all dictionary entries with cost as the sorting variable
-    heapq.heappush(priority_list, [start_node.cost, start_node])  # This Data structure will prioritize the node to be explored which has less cost.
+    # Set to store explored nodes
+    explored_nodes = set()  
+    # Priority queue for storing nodes based on their cost
+    priority_queue = PriorityQueue()  
 
-    all_nodes = []  # stores all nodes that have been traversed, for visualization purposes.
+    # Add start node to the priority queue
+    priority_queue.put((start_node.cost, start_node))  
 
-    while len(priority_list) != 0:
-        # Popping the first element in the priority list to create child nodes for exploration
-        present_node = (heapq.heappop(priority_list))[1]
+    # stores all nodes that have been traversed, for visualization purposes.
+    all_nodes = []  
+
+    while not priority_queue.empty():
+        # Popping the first element in the priority queue to create child nodes for exploration
+        cost, present_node = priority_queue.get()
         # Appending all child nodes so that the explored region of the map can be plotted.
         all_nodes.append([present_node.x, present_node.y])
         # Creating a dict key for identification of node individually
         present_id = key(present_node)
-        # The program will exist if the present node is the goal node
+        # The program will exit if the present node is the goal node
         if Check_goal(present_node, goal_node):
             goal_node.parent_id = present_node.parent_id
             goal_node.cost = present_node.cost
@@ -165,9 +173,7 @@ def dijkstra(start, goal, obs_space):
         if present_id in explored_nodes:
             continue
         else:
-            explored_nodes[present_id] = present_node
-        # Deleting the node from the open nodes list because it has been explored and further its child nodes will be generated
-        del unexplored_nodes[present_id]
+            explored_nodes.add(present_id)
         # For all actions in action set, a new child node has to be formed if it is not already explored
         for move in moves:
             x, y, cost = move(present_node.x, present_node.y, present_node.cost)
@@ -184,21 +190,23 @@ def dijkstra(start, goal, obs_space):
                 if new_node.cost < unexplored_nodes[new_node_id].cost:
                     unexplored_nodes[new_node_id].cost = new_node.cost
                     unexplored_nodes[new_node_id].parent_id = new_node.parent_id
+                    priority_queue.put((new_node.cost, new_node))
             else:
                 unexplored_nodes[new_node_id] = new_node
-
-            heapq.heappush(priority_list, [new_node.cost, new_node])
+                priority_queue.put((new_node.cost, new_node))
 
     return all_nodes, 0, 0
 
-########### BACKTRACK AND GENERATE SHORTEST PATH ############
+
+# backtrack and generate the shortest path
 
 def Backtrack(goal_node):
     x_path = []
     y_path = []
     x_path.append(goal_node.x)
     y_path.append(goal_node.y)
-    total_cost = goal_node.cost  # Initialize total cost
+    # Initialize total cost
+    total_cost = goal_node.cost  
 
     parent_node = goal_node.parent_id
     while parent_node != -1:
@@ -211,17 +219,18 @@ def Backtrack(goal_node):
 
     return x_path, y_path, total_cost  # Return total cost along with path coordinates
 
-######### CALLING ALL MY FUNCTIONS TO IMPLEMENT DIJKSTRA ALGORITHM ON A POINT ROBOT ###########
+
+# calling all the functions to implement dijkstra algorithm
 
 if __name__ == '__main__':
     obs_space = Configuration_space(image)
 
-    #### Taking start node coordinates as input from user #####
+   #### Taking start node coordinates as input from user #####
     while True:
         start_coordinates = input("Enter coordinates for Start Node (x y): ")
         try:
             s_x, s_y = map(int, start_coordinates.split())
-            if not Validity(s_x, s_y, obs_space) or image[s_y][s_x].tolist() == OBS_COLOR or image[s_y][s_x].tolist() == PADDING_COLOR:
+            if not Validity(s_x, s_y, obs_space) or image[s_y][s_x].tolist() == OBS_COLOR or image[s_y][s_x].tolist() == BORDER_COLOR:
                 print("Start node is within an obstacle or outside the map boundaries. Please enter valid coordinates.")
                 continue
             break
@@ -230,17 +239,16 @@ if __name__ == '__main__':
 
     # Taking Goal Node coordinates as input from user
     while True:
-        while True:
-            end_coordinates = input("Enter coordinates for Goal Node (x y): ")
-            try:
-                e_x, e_y = map(int, end_coordinates.split())
-                if not Validity(e_x, e_y, obs_space) or image[e_y][e_x].tolist() == OBS_COLOR or image[e_y][e_x].tolist() == PADDING_COLOR:
-                    print("Goal Node is within an obstacle or outside the map boundaries. Please enter valid coordinates.")
-                    continue
-                break
-            except ValueError:
-                print("Invalid input format. Please enter two integers separated by space.")
-        break
+        end_coordinates = input("Enter coordinates for Goal Node (x y): ")
+        try:
+            e_x, e_y = map(int, end_coordinates.split())
+            if not Validity(e_x, e_y, obs_space) or image[e_y][e_x].tolist() == OBS_COLOR or image[e_y][e_x].tolist() == BORDER_COLOR:
+                print("Goal Node is within an obstacle or outside the map boundaries. Please enter valid coordinates.")
+                continue
+            print("Processing...")
+            break
+        except ValueError:
+            print("Invalid input format. Please enter two integers separated by space.")
 
     # Define start and goal nodes
     start_node = Node(s_x, map_height - s_y, 0, -1)  # Start node with cost 0 and no parent
@@ -252,7 +260,6 @@ if __name__ == '__main__':
     if found_goal:
         # Generate shortest path
         x_path, y_path, total_cost = Backtrack(goal_node)
-        print("Shortest Path: ", x_path, y_path)
         print("Total Cost:", total_cost)
         print("Time taken to find the goal node:", time_taken, "seconds")
     else:
@@ -268,7 +275,7 @@ if __name__ == '__main__':
 
         # Draw shortest path
         for i in range(len(x_path) - 1):
-            cv2.line(image_with_path, (x_path[i], y_path[i]), (x_path[i + 1], y_path[i + 1]), (255, 0, 0), 2)
+            cv2.line(image_with_path, (x_path[i], y_path[i]), (x_path[i + 1], y_path[i + 1]), (0, 255, 0), 2)
 
         # Draw start and end points
         cv2.circle(image_with_path, (s_x,map_height- s_y), 5, (0, 255, 0), -1)  # Green circle for start point
